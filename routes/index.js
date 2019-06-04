3b74de256ff1e6feb4a3b55b6974e55c5c265d2d
Connor Mason <cnnr@icloud.com>
2019-06-04 14:15:23 -0500

    Converting project to Express app

---
+++ routes/index.js
@@ -0,0 +1,74 @@
+var dotenv = require('dotenv').config();
+var express = require('express');
+var router = express.Router();
+const { check,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+const API_KEY = process.env.MAILGUN_KEY;
+const DOMAIN = process.env.MAILGUN_DOMAIN;
+const mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+
+const nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Abby and Connor are getting married.' });
+});
+
+/* GET details page. */
+router.get('/details', function(req, res, next) {
+    res.render('details', { title: 'Details' });
+});
+
+/* GET registry page. */
+router.get('/registry', function(req, res, next) {
+    res.render('registry', { title: 'Registry' });
+});
+
+/* GET RSVP page. */
+router.get('/rsvp', function(req, res, next) {
+    res.render('rsvp', { title: 'RSVP' });
+});
+
+router.post('/', [
+	check('first', 'Please enter normal characters only.').isAscii().trim(),
+	check('first', 'Please enter your first name.').isLength({min:1}).trim(),
+	check('last', 'Please enter normal characters only.').isAscii().trim(),
+	check('last', 'Please enter your last name.').isLength({min:1}).trim(),
+	check('email', 'Please enter an email address.').isLength({min:1}).trim(),
+	check('email', 'Please enter a valid email address.').isEmail().trim(),
+	check('message', 'Please enter a message.').isLength({min:1}).trim()
+	], function(req, res, next) {
+
+		// deal with errors from the form validation
+		const errors = validationResult(req);
+  		if (!errors.isEmpty()) {
+    		return res.status(422).json({ errors: errors.array() });
+  		}
+
+  		// if no validation errors, then create the email
+		let data = {
+			from: req.body.email,
+			to: process.env.MAILGUN_TO_ADDRESS,
+			subject: 'A Connote request from ' + req.body.email,
+			text: req.body.message
+		};
+
+		//send the email
+		mailgun.messages().send(data, (error, body) => {
+			if(error) {
+				console.log(error);
+				res.redirect('/');
+			} else {
+				console.log('An email from ' + req.body.email + ' was sent to ' + process.env.MAILGUN_TO_ADDRESS);
+				res.redirect('/thanks');
+			}
+		});
+
+});
+
+router.get('/thanks', function(req, res, next) {
+	res.render('includes/thanks');
+})
+
+
+module.exports = router;
